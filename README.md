12f5c866b4c33b5f8932da4732dc4bbd74927a75
theFrawley <mattfrawley1@gmail.com>
2020-03-03 23:01:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pixi-play
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pixi-play)
---
+++ index.js
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from 'react';
+import { render } from 'react-dom';
+import * as PIXI from 'pixi.js'
+import './style.css';
+
+function App () {
+
+  const canvas = useRef(null);
+
+  const pixiApp = useRef(
+    new PIXI.Application({
+      view: canvas.current,
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  )
+
+  useEffect(()=> {
+    addSpriteToStage()
+  },[])
+
+  const addSpriteToStage = () => {
+    // Create Texture and then Sprite
+    const texture = PIXI.Texture.from('');
+    const sprite = new PIXI.Sprite(texture);
+    pixiApp.current.stage.addChild(sprite);
+  }
+
+    return (
+      <main>Hello I haven't started this yet!!
+      
+        <canvas ref={canvas} style={{border: '1px solid red'}} />
+      
+      </main>
+    );
+}
+
+render(<App />, document.getElementById('root'));
